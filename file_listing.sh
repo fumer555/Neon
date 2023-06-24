0515bbc686d5309f4fbf5f03354b700a6629dca3
Frank <zi.h.yin@mail.mcgill.ca>
2023-06-24 15:29:25 -0400

    directories listing

--- file_listing.sh
+++ file_listing.sh
@@ -2,10 +2,11 @@
 
 output_file="file_list.txt"
 
+
 # Clear the output file
 > "$output_file"
 
-# Find all files in the current directory and its subdirectories
-find . -type f -print >> "$output_file"
+# Find all directories in the current directory and its subdirectories
+find . -type d -print >> "$output_file"
 
-echo "File list generated and saved to $output_file."
+echo "Directory list generated and saved to $output_file."
