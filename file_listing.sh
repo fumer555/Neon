3146b9457c8ca332f6dc417350592ae94924c92b
Frank <zi.h.yin@mail.mcgill.ca>
2023-06-24 15:37:16 -0400

    src files

--- file_listing.sh
+++ file_listing.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 output_file="file_list.txt"
-
+subdirectory="$1"
 
 # Clear the output file
 > "$output_file"
 
-# Find all directories in the current directory and its subdirectories
-find . -type d -print >> "$output_file"
+# Change directory to the specified subdirectory
+cd "$subdirectory"
 
-echo "Directory list generated and saved to $output_file."
+# Find all files in the current directory and its subdirectories
+find . -type f -exec echo "{}" >> "../$output_file" \;
+
+echo "File list generated and saved to $output_file."
